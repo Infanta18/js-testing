c0369109a030d80f189159cb260e0c5b538d069b
Infanta18 <infantakumar0@gmail.com>
2025-01-07 21:44:31 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,23 @@
+let a=10;
+let b=20;
+const c=a+b;
+console.log(a);
+console.log(b);
+console.log(c);
+
+var d='infanta';
+var e="peaula";
+console.log(d+e);
+
+
+ var f=(5+6)*10;
+ console.log(f);
+
+var g="vino"+" "+"infanta";
+console.log(g)
+
+var LastName,LastName;
+var lastname="vino";
+var lastname="infanta";
+console.log(lastname)
+

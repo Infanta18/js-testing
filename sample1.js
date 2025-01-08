ca895b89a78558102cf43e5e46440b95867df818
Infanta18 <infantakumar0@gmail.com>
2025-01-08 22:39:46 +0530

    Add files via upload

---
+++ sample1.js
@@ -0,0 +1,93 @@
+//declare x,give it the value of 5
+var x=5
+console.log(x)
+
+//declare x,give it the value of 5
+var x=5
+// declare y,give it the value of x+2
+var y=x+2
+console.log(y)
+
+/*
+ooty apple
+*/
+var fruit="apple"
+console.log(fruit)
+/*
+salem mango
+*/
+var fruit="mango"
+console.log(fruit)
+
+var price=1000
+var product="rice"
+var tax=10
+console.log(product)
+var total=price+tax
+console.log(total)
+
+// a is a 5% tax
+var  a=5
+var price=1000
+var product="rice"
+var discount=10
+console.log(product)
+var total=(price+discount)/a
+console.log(total)
+
+var x=10
+y=10
+a=10
+f=10
+c=10
+g=10
+x+=20
+y-=20
+a*=20
+f/=10
+c%=20
+g**=5
+console.log(x)
+console.log(y)
+console.log(a)
+console.log(f)
+console.log(c)
+console.log(g)
+
+{
+    let x=5
+    let y=++x
+    console.log(x)
+}
+
+{
+    let x=5
+    let y=++x
+    console.log(y)
+}
+
+{
+    let x=5
+    let y=x++
+    console.log(x)
+}
+
+{
+    let x=5
+    let y=x++
+    console.log(y)
+}
+
+{
+    let x=5
+    let y=x++
+    console.log(y)
+    console.log(x)
+}
+
+{
+    let x=5
+    let y=x--
+    console.log(y)
+    console.log(x)
+}

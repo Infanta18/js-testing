9d4ec75f977da50919f82766c3b030977e0e5955
Infanta18 <infantakumar0@gmail.com>
2025-01-09 22:11:32 +0530

    Add files via upload

---
+++ sample2.js
@@ -0,0 +1,41 @@
+{
+let text1=10
+let text2=20
+let c=text1<text2
+console.log(c)
+} 
+
+{
+    let text1=189
+    let text2=20
+    let c=text1<text2
+    console.log(c)
+} 
+
+{
+    let text1=189
+    let text2=20
+    let c=text1<text2
+    console.log("Is 189 less than 20?"+c)
+    let text3=189
+    let text4=20
+    let d=text1>=text2
+    console.log("Is 189 great than 20?"+d)
+}
+
+var x=5+5
+var y="5"+5
+var z="hello"+5
+console.log(x,y,z)
+
+var a
+console.log(a)
+console.log(typeof("apple"))
+console.log(typeof("5"))
+console.log(typeof(5))
+var a=true
+console.log(a)
+var a=false
+console.log(a)
+console.log(typeof("true"))
+console.log(typeof(true))

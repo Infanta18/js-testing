a97824e594ab8d374f306035c676ff85410280e2
Infanta18 <infantakumar0@gmail.com>
2025-01-10 22:16:41 +0530

    Add files via upload

---
+++ sample3.js
@@ -0,0 +1,88 @@
+function abc()
+{
+    console.log("hello")
+}
+abc()
+
+function phone()
+{
+    console.log("oppo")
+}
+phone()
+function phone()
+{
+    console.log("apple")
+}
+phone()
+function phone()
+{
+    console.log("redmi")
+}
+phone()
+function hi()
+{
+    console.log("oppo")
+}
+hi()
+function hello()
+{
+    console.log("redmi")
+}
+hello()
+function win()
+{
+    console.log("apple")
+}
+win()
+
+var a=10
+var b=20
+function add()
+{
+    console.log(a+b)
+}
+add()
+
+var a=10
+var b=20
+function arith()
+{
+    console.log(a+b)
+    console.log(a-b)
+    console.log(a*b)
+    console.log(a/b)
+    console.log(a%b)
+}
+arith()
+
+var fcolor="red"
+var fplace="ooty"
+var fmovie="joe"
+function favourite()
+{
+    console.log("favourite color:"+fcolor)
+    console.log("favourite place:"+fplace)
+    console.log("favourite movie:"+fmovie)
+}
+favourite()
+var fcolor="red"
+var fplace="ooty"
+var fmovie="joe"
+function favourite()
+{
+    console.log("favourite color:"+"red")
+    console.log("favourite place:"+"ooty")
+    console.log("favourite movie:"+"joe")
+}
+favourite()
+
+function add(a,b)
+{
+    console.log("print the parameters:"+a+b)
+}
+add()
+function add(a,b)
+{
+    console.log("print the parameters:"+(a+b))
+}
+add(a,b)
